4f7792be92c15bed57f05c9bdfbd3b96d6ef2a4f
Kevin Choy <kevinchoy22@gmail.com>
2019-01-22 12:16:25 -0800

    complete full redux cycle

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,8 +1,40 @@
-const todosReducer = (state = {}, action) => {
+import {RECEIVE_TODOS} from '../actions/todo_actions';
+import {RECEIVE_TODO} from '../actions/todo_actions';
+
+import merge from 'lodash/merge';
+
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  let nextState = {};
+
   switch(action.type) {
+    case RECEIVE_TODOS:
+      action.todos.forEach(todo => {
+        nextState[todo.id] = todo;
+      });
+      return nextState;
+    case RECEIVE_TODO:
+      const newTodo = {[action.todo.id]: action.todo};
+      return newTodo;
     default: 
       return state; 
-  }
-};
+    }
+  };
+  
 
 export default todosReducer; 
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import {receiveTodo, receiveTodos} from './actions/todo_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   window.store = store;
+
+  const newTodos = [
+    {
+      id: 1,
+      title: 'mod car',
+      body: 'fire',
+      done: false
+    },
+    {
+      id: 2,
+      title: 'whipping',
+      body: 'krispy creme whip',
+      done: true
+    },
+  ];
+
+  console.log(store.getState()); // should return default state object
+  store.dispatch(receiveTodo({ id: 3, title: 'New Todo' }));
+  console.log(store.getState()); // should include the newly added todo
+  store.dispatch(receiveTodos(newTodos));
+  console.log(store.getState()); // should return only the new todos
+
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Todos app</h1>, root);
 });
